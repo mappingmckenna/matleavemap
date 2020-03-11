ea995441bf4652ae15bfc1bdf91263da362e60f4
mappingmckenna <elise.s.mck@gmail.com>
2020-03-11 17:48:17 +1100

    Update config.js

--- src/vanilla-js/config.js
+++ src/vanilla-js/config.js
@@ -15,10 +15,10 @@ var config = {
             image: './path/to/image/source.png',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             location: {
-                center: [-122.418398, 37.759483],
-                zoom: 13.5,
-                pitch: 60,
-                bearing: 0
+                center: { lon: 151.11490, lat: -34.03661 },
+                zoom: 14.11,
+                pitch: 54.50,
+                bearing: 0.00
             },
             onChapterEnter: [
                 // {
